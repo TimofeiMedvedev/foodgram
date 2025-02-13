57f8cc8ea330bef5c9ccc39cfd3a2c0b7bd34e19
TimofeiMedvedev <medtv78@gmail.com>
2025-02-13 19:17:38 +0300

    add pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -187,14 +187,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ).all()
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrive',]:
+        if self.action in ['list', 'retrive', ]:
             return RecipeReadSerializer
         return RecipeCreateSerializer
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy',
                            'favorite', 'download_shopping_cart',
-                           'shopping_cart',]:
+                           'shopping_cart', ]:
             return [IsAuthenticated(), IsAuthorOrReadOnly()]
         return [AllowAny()]
 
